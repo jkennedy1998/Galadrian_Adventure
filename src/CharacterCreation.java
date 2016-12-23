be9e19cb179a69d124660ea58cd6d842afb0a345
jnebl7236 <jneblparcore@gmail.com>
2016-12-22 18:55:30 -0600

    messed with creation 4#

--- src/CharacterCreation.java
+++ src/CharacterCreation.java
@@ -10,7 +10,30 @@ public class CharacterCreation {
     }
     public static void race()
     {
-
+        int x = 0;
+        int y = 0;
+        do {
+           if (y == 0)
+               System.out.println("When given the option enter your desired race");
+           else
+               System.out.println("When given the option enter your desired race not some random shit like you apparently did");
+           String races = "Your race options are Human, Elf, or Dwarf";
+           String standard =" please enter the exact name of your desired race";
+           System.out.println(races);
+           String rac = IO.getString(standard);
+           if (rac.equalsIgnoreCase("Dwarf")){
+               x = 1;
+           }
+           else if (rac.equalsIgnoreCase("Elf")){
+               x = 1;
+           }
+           else if (rac.equalsIgnoreCase("Human")){
+               x = 1;
+           }
+           else{
+               y = 1;
+           }
+        }while (x == 0);
     }
     public static void role()
     {
