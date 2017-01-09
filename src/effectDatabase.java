a6a20b69fab704cfc9d93bd515c50e9c17bd4eb2
jkennedy1998 <jkennedy1998@gmail.com>
2017-01-09 12:05:13 -0600

    added in a class which does nothing. bare bones for effect statuses

---
+++ src/effectDatabase.java
@@ -0,0 +1,14 @@
+/**
+ * Created by student5 on 1/9/17.
+ */
+public class effectDatabase {
+    //effect argument notes!
+    //effect name
+    //damage mult mod
+    // speed mult mod
+    // defence mult mod
+    // accuracy mult mod
+    //health effected each turn
+    
+
+}
