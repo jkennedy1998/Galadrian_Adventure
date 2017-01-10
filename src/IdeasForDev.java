e310deb1229f27e40401d6e324b5beafa77be9b1
jkennedy1998 <jkennedy1998@gmail.com>
2017-01-10 12:02:23 -0600

    comment file. not a useable class, just for writing ideas down in case you cant do them or have no time.
    added worn item idea
    added monster type idea

---
+++ src/IdeasForDev.java
@@ -0,0 +1,18 @@
+//hey! this is just for posting ideas to implement in the game without clogging up other class's data.
+//formatting below
+//new idea
+//  aspects of new idea
+//      personal comment about idea
+
+//items that players will get
+//  items will be kept equipped and not be usable.
+//  will edit stats and modifiers temporarily while they are equipted.
+//  they can be broken (armor broken one it receives a certain ammount of damage)
+//  they are dropped by monsters or are dropped by the story class through the main quest.
+//  items that are non usable will be stored in itemDirectory.
+//  new argument for items to declare if item is usable.
+
+//atacks and items affecting certain monster types more than others
+//  bug like creatures (snakes spiders and bats) will be affected by an item more than a giant. (more dam)
+//  new arg in monster where we do a integer id of a monster type. (ie 1 = bugLike, 2 = largeHumanoid)
+//      would make a higher level of tactical thinking for the player.
