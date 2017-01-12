6c9d4b864cb815751f97d84ad905fcd87e9711c8
jkennedy1998 <jkennedy1998@gmail.com>
2017-01-12 11:57:09 -0600

    got rid of done ideas

--- src/IdeasForDev.java
+++ src/IdeasForDev.java
@@ -16,7 +16,3 @@
 //  bug like creatures (snakes spiders and bats) will be affected by an item more than a giant. (more dam)
 //  new arg in monster where we do a integer id of a monster type. (ie 1 = bugLike, 2 = largeHumanoid)
 //      would make a higher level of tactical thinking for the player.
-
-//changing accuracy
-//  changing to accuracy percentages as a double. or an int out of 100
-//      would make it easier to scale monsters and players. makes modifiers do more and less in extreem cases.
