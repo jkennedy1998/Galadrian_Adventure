d5c9d8dda038d085f62892cc2fabaf49d9dc7dbc
jkennedy1998 <jkennedy1998@gmail.com>
2017-01-19 17:49:42 -0600

    got rid of done idea

--- src/IdeasForDev.java
+++ src/IdeasForDev.java
@@ -4,10 +4,6 @@
 //  aspects of new idea
 //      personal comment about idea
 
-//items that players will get
-//  they can be broken (armor broken one it receives a certain ammount of damage)
-//  they are dropped by monsters or are dropped by the story class through the main quest.
-
 //attacks and items affecting certain monster types more than others
 //  bug like creatures (snakes spiders and bats) will be affected by an item more than a giant. (more dam)
 //  new arg in monster where we do a integer id of a monster type. (ie 1 = bugLike, 2 = largeHumanoid)
