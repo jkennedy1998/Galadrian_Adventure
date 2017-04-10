9948b0f8472137982c65687e6f3e422b4cb5da3a
jkennedy1998 <jkennedy1998@gmail.com>
2017-04-09 21:01:43 -0500

    added ideas

--- src/Battle/IdeasForDev.java
+++ src/Battle/IdeasForDev.java
@@ -14,3 +14,17 @@
 
 //enemies become enraged sometimes when the fall below 20% health. or become fearfull
     //basee off of their behavior in cartography? hat would be sick
+
+//add more nonwalls to use in puzzles or to prevent players from going into an area that they are not ready for
+//  buttons(on walls rhat you have to bump into)
+//  locked doors and linked keys
+//  lock picks?!?!
+//  breakable terrain (like small trees or boulders that you can break with axes or shovels)
+//  traped doors (on ground that you fall through and ones that are hidden in walls.)
+//  something players can collect
+
+//revamp items to go off of a weight based system
+//  would allow players to gather more or less items based on what you need (more potions or more armor)
+//    make armor an item that you can use to put on
+//    make items able to be used outside the battlefied
+//      would allow for some pretty cool lore based puzzles
